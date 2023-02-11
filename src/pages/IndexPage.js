e6fc4d90a16b7de57f5753ad7217bd8264213f68
Praveen <2pkashyap2001@gmail.com>
2023-02-11 20:36:11 +0530

    first commit

---
+++ src/pages/IndexPage.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom"
+export default function IndexPage(){
+    return(<>
+Index page should go here
+    </>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'rgb(1, 122, 243)',
+      },
+    },
+  },
+  plugins: [],
+}
